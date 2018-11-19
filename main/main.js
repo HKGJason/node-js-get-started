490d8224b7174b32275208bb77182aca276b5d9a
Jason Liu <jason.liu@oocl.com>
2018-11-19 16:47:05 +0800

    all done

--- main/main.js
+++ main/main.js
@@ -1,9 +1,26 @@
 // Write your cade below:
-function caclRemaindar() {};
+function caclRemaindar(i,j) {
+	return i%j;
+};
 
-function caclSum() {};
+function caclSum(arr) {
+	var count = 0;
+	for (var i = 0; i<arr.length;i++){
+		count = count + arr[i];
+	}
+	return count;
+	
+};
 
-function caclSumInConditon() {};
+function caclSumInConditon(arr, num) {
+	var count = 0;
+	for (var i = 0; i<arr.length;i++){
+		if (arr[i]<num)
+		count = count + arr[i];
+	}
+	return count;
+	
+};
 
 module.exports = {
     caclRemaindar,
